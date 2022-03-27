250e4b1b68c588a2936b06c908f94293c25985c2
yo8ssv <50114461+yo8ssv@users.noreply.github.com>
2022-03-27 03:08:32 +0200

    Create config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -5,7 +5,7 @@
 
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
+#define SPEEDLIMIT_NORMAL 25
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA
